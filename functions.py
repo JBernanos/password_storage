7c6b0fca7c7b248d2345866c45642608f5eae2fe
JBernanos <jp.bf@icloud.com>
2021-07-20 18:55:59 -0300

    changing files

--- functions.py
+++ functions.py
@@ -1,64 +1,93 @@
 import sys
-from time import sleep
 from random import randint
 from tkinter import *
-from tkinter import filedialog 
 from tkinter import messagebox
 
-def data():
-    lowercase_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+lowercase_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
                          'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    uppercase_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+
+uppercase_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
                          'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    special_characters = [
-        '!', '?', '{', '[', '(', '}', ']', ')', '.', ',', '<', '>', '-', '+', '=', '*', '/']
-    password, length = password_length()
-    create_password(uppercase_letters, lowercase_letters, numbers,
-                    special_characters, password, length)
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+special_characters = ['!', '?', '{', '[', '(', '}', ']', ')', '.', ',', '<', '>', '-', '+', '=', '*', '/']
 
 
-def password_length():
-    try:
-        length = int(input('Choose the length for your password: '))
-
-        if length < 8:
-            print('The length of your password should be greater then 8.')
-            password_length()
-
-        else:
-            password = ['0'] * length
-            return password, length
-
-    except ValueError:
-        print('Incorrect input, you will be redirected to menu.')
-        sleep(0.5)
+def not_agp_password():
+    isRandom = False
+    top_record_data(isRandom)
 
 
-def create_password(uppercase_letters, lowercase_letters, numbers, special_characters, password, length):
+def agp_password():
+    isRandom = True
+    top_record_data(isRandom)
+
+
+def top_record_data(isRandom):
+    global top
+    top = Toplevel()
+    top['bg']='#fb0'
+
+    record_name_label = Label(top, text="Record Name: ").grid(row=0, column=0)
+    record_name = Entry(top, width=15)
+    record_name.grid(row=0, column=1)
+
+    login_label = Label(top, text="Login: ").grid(row=1, column=0)
+    login = Entry(top, width=15)
+    login.grid(row=1, column=1)
+
+    if isRandom:
+        password_label = Label(top, text="Password Size: ").grid(row=2, column=0)
+        password_size = Entry(top, width=15)
+        password_size.grid(row=2, column=1)
+        submit_btn = Button(top, text="Submit", command=lambda: validate_password_size(password_size, top, record_name, login)).grid(row=3, column=0)
+    
+    else:
+        password_label = Label(top, text="Password: ").grid(row=2, column=0)
+        password = Entry(top, width=15)
+        password.grid(row=2, column=1)
+        submit_btn = Button(top, text="Submit", command=lambda: store_password(str(password.get()), record_name, login)).grid(row=3, column=0)
+
+
+def validate_password_size(password_size, top, record_name, login):
+    value = password_size.get()
+    value = int(value)
+
+    if value < 8:
+        top.destroy()
+        messagebox.showerror("[ERROR]", "Password Size should be greater or equal than 8!")
+        top_record_data()
+    
+    else:
+        password = [0] * value
+        create_password(password, value, record_name, login)
+
+
+def create_password(password, password_size, record_name, login):
     counter1 = counter2 = counter3 = counter4 = 0
     position = 0
 
-    while counter1 < 2 or counter2 < 2 or counter3 < 2 or counter4 < 2 or position < length:
+    while counter1 < 2 or counter2 < 2 or counter3 < 2 or counter4 < 2 or position < password_size:
         option = randint(1, 4)
 
         if option == 1 and counter1 < 2:
-            insert_upper_case_letters(uppercase_letters, password, position)
+            insert_upper_case_letters(password, position)
             counter1 += 1
             position += 1
 
         elif option == 2 and counter2 < 2:
-            insert_lower_case_letters(lowercase_letters, password, position)
+            insert_lower_case_letters(password, position)
             counter2 += 1
             position += 1
 
         elif option == 3 and counter3 < 2:
-            insert_numbers(numbers, password, position)
+            insert_numbers(password, position)
             counter3 += 1
             position += 1
 
         elif option == 4 and counter4 < 2:
-            insert_special_characters(special_characters, password, position)
+            insert_special_characters(password, position)
             counter4 += 1
             position += 1
 
@@ -66,47 +95,45 @@ def create_password(uppercase_letters, lowercase_letters, numbers, special_chara
             option = randint(1, 4)
 
             if option == 1:
-                insert_upper_case_letters(
-                    uppercase_letters, password, position)
+                insert_upper_case_letters(password, position)
                 position += 1
 
             elif option == 2:
-                insert_lower_case_letters(
-                    lowercase_letters, password, position)
+                insert_lower_case_letters(password, position)
                 position += 1
 
             elif option == 3:
-                insert_numbers(numbers, password, position)
+                insert_numbers(password, position)
                 position += 1
 
             elif option == 4:
-                insert_special_characters(
-                    special_characters, password, position)
+                insert_special_characters(password, position)
                 position += 1
-
+                
     password = list_to_string(password)
-    store_password(password)
+    store_password(password, record_name, login)
+    
 
-
-def insert_upper_case_letters(uppercase_letters, password, position):
+def insert_upper_case_letters(password, position):
     elem = randint(0, 25)
     password[position] = uppercase_letters[elem]
     return password
 
 
-def insert_lower_case_letters(lowercase_letters, password, position):
+def insert_lower_case_letters(password, position):
     elem = randint(0, 25)
     password[position] = lowercase_letters[elem]
     return password
 
 
-def insert_numbers(numbers, password, position):
+def insert_numbers(password, position):
     elem = randint(0, 9)
     password[position] = numbers[elem]
     return password
 
 
-def insert_special_characters(special_characters, password, position):
+def insert_special_characters(password, position):
+
     elem = randint(0, 16)
     password[position] = special_characters[elem]
     return password
@@ -121,48 +148,53 @@ def list_to_string(password):
     return string
 
 
-def store_password(password):
-    print('====================================')
-    record_name = str(input('Record Name: ')).upper()
-    login = str(input('Login: '))
+def store_password(password, record_name, login):
+    record_name = record_name.get()
+    record_name = record_name.upper()
+    login = login.get()
+
+    top.destroy()
 
     with open('data.txt', 'a') as file:
         file.write(
             f'Record Name: {record_name} \nLogin: {login} \nPassword: {password} \n')
         file.write('====================================\n')
 
-    sleep(0.5)
+    messagebox.showinfo("[SUCCESS]", "Record Successfully Added")
 
 
-def add_record():
-    password = str(input('Password: '))
-    store_password(password)
+def change_record():
+    deleteRecord = False
+    top_record_name(deleteRecord)
 
 
-def edit_record():
-    delete_record()
-    print('====================================')
-    option()
+def delete_record():
+    deleteRecord = True
+    top_record_name(deleteRecord)
 
 
-def option():
-    choose = int(
-        input('1- Auto Generated Password \n2- Type your own password \nChoose:'))
-    if choose == 1:
-        data()
+def top_record_name(deleteRecord):
+    global top
+    top = Toplevel()
+    top['bg']='#fb0'
 
-    elif choose == 2:
-        add_record()
+    if deleteRecord:
+        record_name_label = Label(top, text="Record Name: ").grid(row=0, column=0)
+        record_name = Entry(top, width=15)
+        record_name.grid(row=0, column=1)
+
+        submit_btn = Button(top, text="Submit", command=lambda: remove_record(str(record_name.get()))).grid(row=3, column=0)
 
     else:
-        print('Invalid option!')
-        choose = 0
-        option()
+        record_name_label = Label(top, text="Record Name: ").grid(row=0, column=0)
+        record_name = Entry(top, width=15)
+        record_name.grid(row=0, column=1)
 
-    
-def delete_record():
-    record_name = str(
-        input('Enter the name of the record to be deleted: ')).upper()
+        submit_btn = Button(top, text="Submit", command=lambda: edit_record(str(record_name.get()))).grid(row=3, column=0)
+        
+
+def remove_record(record_name):
+    record_name = record_name.upper()
     record_on_data = 0
     counter = 0
     with open('data.txt', 'r') as file:
@@ -187,6 +219,16 @@ def delete_record():
         fail_msg()
 
 
+def sucess_msg():
+    messagebox.showinfo("Delete Record", "Record Sucesseful removed!")
+    top.destroy()
+
+
+def fail_msg():
+    messagebox.showinfo("Delete Record", "Record not found!")
+    top.destroy()
+
+
 def show_records(): 
     top = Toplevel()
     top['bg']='#fb0'
@@ -199,12 +241,3 @@ def show_records():
     data = file.read()
     txtarea.insert(END, data)
     file.close()
-
-
-def sucess_msg():
-    messagebox.showinfo("Delete Record", "Record Sucesseful removed!")
-
-
-def fail_msg():
-    messagebox.showinfo("Delete Record", "Record not found!")
-
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import random
 from functions import *
 
 root = Tk()
@@ -11,23 +12,23 @@ root.rowconfigure(0, weight=1)
 container = Frame(root)
 container.grid(row=0, column=0)
 
+
 def exit_program():
     root.quit()
     print('\nShutting the program down, wait a second.')
-    sleep(0.5)
+    # sleep(0.5)
     sys.exit()
 
+new_record_apg_btn = Button(container, text="NEW RECORD(APG)", command=agp_password, padx=20, pady=10, width=20).grid(row=0, column=0)
 
-new_record_apg_btn = Button(container, text="NEW RECORD(APG)", command=data, padx=20, pady=10, width=20).grid(row=0, column=0)
+new_record_btn = Button(container, text="NEW RECORD", command=not_agp_password, padx=20, pady=10, width=20).grid(row=1, column=0)
 
-new_record_btn = Button(container, text="NEW RECORD", command=add_record, padx=20, pady=10, width=20).grid(row=1, column=0)
-
-edit_record_btn = Button(container, text="EDIT RECORD", command=edit_record, padx=20, pady=10, width=20).grid(row=2, column=0)
+# edit_record_btn = Button(container, text="EDIT RECORD", command=change_record, padx=20, pady=10, width=20).grid(row=2, column=0)
 
 delete_record_btn = Button(container, text="DELETE RECORD", command=delete_record, padx=20, pady=10, width=20).grid(row=3, column=0)
 
 show_record_btn = Button(container, text="SHOW RECORDS", command=show_records, padx=20, pady=10, width=20).grid(row=4, column=0)
 
-exit_program_btn = Button(container, text="EXIT PROGRAM", command=exit_program, padx=20, pady=10, width=20   ).grid(row=5, column=0)
+exit_program_btn = Button(container, text="EXIT PROGRAM", command=exit_program, padx=20, pady=10, width=20).grid(row=5, column=0)
 
 root.mainloop()
